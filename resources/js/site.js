c78ff994fd82db0bbe30f260e6640c3ffac6737e
Nate Beaty <nate@clixel.com>
2023-12-09 13:45:23 -0500

    use glide for carousel of image grids

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,3 +1,12 @@
 import GLightbox from 'glightbox';
 import 'glightbox/dist/css/glightbox.css';
+import Glide from '@glidejs/glide'
+import '@glidejs/glide/dist/css/glide.core.css';
+import '@glidejs/glide/dist/css/glide.theme.css';
+
 const lightbox = GLightbox();
+new Glide('.glide', {
+	type: 'carousel',
+	autoplay: 5000,
+	animationDuration: 1500,
+}).mount();
